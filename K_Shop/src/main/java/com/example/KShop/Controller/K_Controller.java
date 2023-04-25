63d361732d539aef230cb201a50cbdb45a2c2fd6
KavinaAshokkumar <727821tuec107@skct.edu.in>
2023-04-25 07:50:13 +0530

    day 5

--- K_Shop/src/main/java/com/example/KShop/Controller/K_Controller.java
+++ K_Shop/src/main/java/com/example/KShop/Controller/K_Controller.java
@@ -1,5 +1,49 @@
 package com.example.KShop.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.KShop.DataBase.Database_table;
+import com.example.KShop.Service.K_Service;
+
+@RestController
 public class K_Controller {
 
+	@Autowired
+	public K_Service kser;
+	
+	@PostMapping("/K_ShopData")
+	public Database_table addDetails(@RequestBody Database_table Data)
+	{
+		return kser.saveInfo(Data);
+	}
+	
+	@GetMapping("/getK_Shop")
+	public List <Database_table> getDetails()
+	{
+		return kser.getInfo();
+	}
+	
+	@PutMapping("/UpdateK_Shop") 
+	public Database_table updateDetails(@RequestBody Database_table Datano)
+	{
+		return kser.updateInfo(Datano);
+	}
+	@DeleteMapping("/deleteKShop/{itemNo}")
+	public String deleteDetails(@PathVariable ("itemNo") int itno)
+    {
+	kser.deleteInfo(itno);
+	return " Item Number " + itno + " is deleted ";
+    }
+
+	
 }
--- K_Shop/src/main/java/com/example/KShop/Service/K_Service.java
+++ K_Shop/src/main/java/com/example/KShop/Service/K_Service.java
@@ -1,5 +1,7 @@
 package com.example.KShop.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,18 @@ public class K_Service {
 		return krep.save(im);
 		
 		}
-	public List<>
+	public List <Database_table > getInfo()
+	{
+		return krep.findAll();
+	}
+	public Database_table updateInfo(Database_table dataK)
+	{
+		return krep.saveAndFlush(dataK);
+	}
+	public void deleteInfo( int itemNo)
+	{
+		krep.deleteById(itemNo);
+	}
+	
 	}
 	
